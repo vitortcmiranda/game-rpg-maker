a90a59e8683faeb410d278ea3df7b2e51da629c6
vitor.miranda <vitor.miranda@movile.com>
2022-08-18 22:01:38 -0300

    initial commit

---
+++ gamerpgmaker-webflux/src/main/kotlin/com/gamerpgmaker/gamerpgmakerwebflux/GamerpgmakerWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.gamerpgmaker.gamerpgmakerwebflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamerpgmakerWebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<GamerpgmakerWebfluxApplication>(*args)
+}
---
+++ gamerpgmaker-webflux/src/test/kotlin/com/gamerpgmaker/gamerpgmakerwebflux/GamerpgmakerWebfluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gamerpgmaker.gamerpgmakerwebflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamerpgmakerWebfluxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
